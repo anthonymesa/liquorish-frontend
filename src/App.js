7069e35899e90d8d00b9210b1acb81904436ad01
Anthony Mesa <amesa@hyperfix.digital>
2022-01-22 22:38:46 -0500

    Set web request to promise

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function httpGet(theUrl)
-{
+function httpGet(theUrl) {
+  return new Promise((resolve, reject) => {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    resolve(xmlHttp.responseText);
+  });
 }
 
 //  Returns the page html to be displayed
@@ -18,23 +19,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img 
-          src={ logo } 
-          className="App-logo" 
-          alt="logo" 
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
         />
 
         <p>
           Liquorish!
         </p>
-        
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          { httpGet('http://liquorish-server.azurewebsites.net/test.js') } learn react
+          {
+            httpGet('http://liquorish-server.azurewebsites.net/test.js')
+          } 
+          learn react
         </a>
       </header>
     </div>
