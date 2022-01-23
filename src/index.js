78ea9c2ed6bf44a90bbdfe8f521642368e3006c5
Anthony Mesa <amesa@hyperfix.digital>
2022-01-23 09:11:34 -0500

    removed unused var in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import DbTestConnection from './Db.js';
 import reportWebVitals from './reportWebVitals';
 
 //  This renders the code generated in the React.StrictMode 
