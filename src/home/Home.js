cb32b751d7a60053e0233ab4f9ed6e40566e5cb3
Anthony Mesa <amesa@hyperfix.digital>
2022-03-14 08:35:44 -0400

    got promises working properly for home

--- src/home/Home.js
+++ src/home/Home.js
@@ -16,26 +16,60 @@ const Header = (props) => {
   )
 }
 
+const handleClickBar = (_bar_data) => {
+  console.log(_bar_data["bar_name"])
+}
+
+const getBarList = new Promise(async (resolve, reject) => {
+  let client_id = sessionStorage.getItem('client_id');
+
+  const url = 'http://liquorish-server.azurewebsites.net/bars/' + client_id;
+
+  const response = await fetch(url);
+  const jsonResponse = await response.json();
+
+  resolve(jsonResponse.value)
+});
+
 const BarList = (props) => {
+  const [bar_list, setBarList] = React.useState([])
+
+  getBarList.then((_bar_list) => {
+    console.log(_bar_list)
+    setBarList(_bar_list);
+  });
+
+  const bar_list_dom = bar_list.map((bar_data) =>
+    <div key={JSON.stringify(bar_data)} className="bar_list_item" onClick={() => { handleClickBar(bar_data) }}>
+      <Row>
+        <h2>{bar_data.bar_name}</h2>
+      </Row>
+      <Row>
+        <p>{bar_data.address_street}, 
+        { bar_data.address_city}, 
+        { bar_data.address_state }, 
+        { bar_data.address_zip }</p>
+      </Row>
+    </div>
+  );
 
   return (
     <div>
-      Barlist
-      {
-        props.bar_list.map((bar_data) => {
-          <div className="bar_list_item" onClick={() => { /*handleClickBar(bar_data)*/ }}>
-            <div>
-              <h2>{bar_data["bar_name"]}</h2>
-            </div>
-            <div>
-              <p>{bar_data["address_street"]}</p>
-            </div>
-          </div>
-        })
-      }
+      {bar_list_dom}
     </div>
   )
 }
+
+const getUserData = new Promise(async (resolve, reject) => {
+  let client_id = sessionStorage.getItem('client_id');
+
+  const url = 'http://liquorish-server.azurewebsites.net/user/' + client_id;
+
+  const response = await fetch(url);
+  const jsonResponse = await response.json();
+
+  resolve(jsonResponse.value)
+});
 
 /**
  * The main home module
@@ -45,41 +79,20 @@ const BarList = (props) => {
  */
 const Home = (props) => {
 
-  const [username, setUsername] = React.useState("");
-  const [bar_list, setBarList] = React.useState([])
+  const [user_data, setUserData] = React.useState({});
 
   useEffect(() => {
-     (async () => {
-      const client_id = sessionStorage.getItem('client_id');
 
-      const url = 'http://liquorish-server.azurewebsites.net/user/' + client_id;
+    getUserData.then((_user_data) => {
+      setUserData(_user_data)
+    })
 
-      const response = await fetch(url);
-      const jsonResponse = await response.json();
-
-      // need to handle the case where this returns in error
-
-      setUsername(jsonResponse.value['username']);
-    })();
-
-    (async () => {
-      const client_id = sessionStorage.getItem('client_id');
-
-      const url = 'http://liquorish-server.azurewebsites.net/bars/' + client_id
-
-      const response = await fetch(url);
-      const jsonResponse = await response.json();
-
-      // need to handle the case where this returns in error
-
-      setBarList(jsonResponse.value)
-    })();
   }, []);
 
   return (
     <div className="root" id="home-root">
-      <Header username={username} />
-      <BarList bar_list={bar_list} />
+      <Header username={user_data["username"]} />
+      <BarList user_id={user_data["id"]} />
     </div>
   )
 }
