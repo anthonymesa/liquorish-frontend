409e7b4898a43549a5a255879048c50d147faaca
anthony mesa <amesa@hyperfix.digital>
2022-03-21 01:33:22 -0400

    created universal headerv2 module and liquorish-footer styling. created AddRto page. Created DrinkView module for reuseability. Updated NewOrder.js to use the new header and added styling. Updated OrderView so that it uses new DrinkView module.

--- src/headerv2/HeaderV2.js
+++ src/headerv2/HeaderV2.js
@@ -1,18 +1,58 @@
+/**
+ * HeaderV2.js
+ * 
+ * Author: Anthony Mesa
+ * 
+ * HeaderV2 is the header module that is used across the application. There are
+ * some functionalitys baked in it to make it useable across the site:
+ * 
+ * - does_nav: Setting this to false removes the 'back' navigation.
+ * - nav_name: The text to be rendered for the back button. Not rendered if does_nav
+ *    is false.
+ * - nav_link: The link to navigate to. This doesn't work if does_nav is false.
+ * - title: A text title to be displayed in the center of the header.
+ * - elements: An array of JSX elements provided by the caller to be displayed
+ *    in the far right section of the header.
+ */
 
 import './HeaderV2.css';
 
-import React from 'react'
-import { Row, Col, Container } from 'react-bootstrap';
+import React, { useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap';
 import { FiChevronLeft } from 'react-icons/fi';
 import { useNavigate } from 'react-router';
 
-const HeaderV2 = ({ does_nav = false, nav_name, nav_link, title, elements }) => {
+const HeaderV2 = ({ does_nav = false, nav_name, nav_link, title, elements = [], unstore = []}) => {
 
   const navigate = useNavigate();
 
+  const [element_row, setElementRow] = React.useState(null)
+
   const handleBack = () => {
-    navigate(nav_link);
+    unstore.forEach((element) => {
+      sessionStorage.removeItem(element);
+    });
+    navigate(nav_link, { replace: true });
   }
+
+  useEffect(async () => {
+
+    console.log("HeaderV2.useEffect([]) - Rendering header!")
+
+    const bootstrapped_element_cols = await elements.map((element) =>
+      <Col className="headerv2-elements-col">
+        {element}
+      </Col>
+    );
+
+    const bootstrapped_element_row = (
+      <Row className="g-0 headerv2-elements-row">
+        {bootstrapped_element_cols}
+      </Row>
+    )
+
+    setElementRow(bootstrapped_element_row)
+  }, []);
 
   return (
     <div className="headerv2">
@@ -27,7 +67,7 @@ const HeaderV2 = ({ does_nav = false, nav_name, nav_link, title, elements }) => 
       {/* This is the title display, it spans the full
           width of the page in the background */}
       <div className="headerv2-title">
-        {title}
+        <h2>{title}</h2>
       </div>
 
       {/* This is a list of custom DOM elements to
@@ -36,8 +76,8 @@ const HeaderV2 = ({ does_nav = false, nav_name, nav_link, title, elements }) => 
           and custom navigations/actions, that sort of
           thing. The dom elements need to be created 
           already as we will be calling them directly. */}
-      <div>
-        {elements}
+      <div className="headerv2-elements">
+        {element_row}
       </div>
     </div>
 
@@ -45,18 +85,3 @@ const HeaderV2 = ({ does_nav = false, nav_name, nav_link, title, elements }) => 
 }
 
 export default HeaderV2;
-
-/*
-<Row className="headerv2">
-      <Col className="col-2">
-        {does_nav &&
-          <div id="header_nav" onClick={() => { handleBack() }}>
-            <FiChevronLeft /> { nav_name }
-          </div>
-        }
-
-      </Col>
-      <Col className="col-10 headerv2-title">
-        { title }
-      </Col>
-    </Row>*/
--- src/new_order/NewOrder.js
+++ src/new_order/NewOrder.js
@@ -2,6 +2,7 @@
 import './NewOrder.css';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router';
+import HeaderV2 from '../headerv2/HeaderV2';
 
 const Header = () => {
 
@@ -26,6 +27,8 @@ const Header = () => {
 
 const NewOrderList = (props) => {
 
+  const navigate = useNavigate();
+
   const [bar_drinks_dom, setBarDrinksDom] = React.useState([]);
 
   const getBarDrinksDom = (_bar_id) => {
@@ -36,15 +39,24 @@ const NewOrderList = (props) => {
       const response = await fetch(url)
       const jsonResponse = await response.json();
 
-      const handleBarDrinkSelection = () => {
-        console.log("NewOrder::NewOrderList.getBarDrinksDom.handleBarDrinkSelection - Clicked!");
+      const handleBarDrinkSelection = (_drink_data) => {
+        sessionStorage.setItem('drink_data', JSON.stringify(_drink_data));
+        navigate('/dashboard/neworder/addrto', { replace: true })
       }
 
       const bar_drinks_dom_generator = await jsonResponse.value.map((drink_data) =>
-        <div key={JSON.stringify(drink_data)} className="bar_drink" onClick={handleBarDrinkSelection}>
+        <div key={JSON.stringify(drink_data)} className="bar_drink" onClick={() => { handleBarDrinkSelection(drink_data); }}>
           <div>
-            <h2>{drink_data["drink_name"]}</h2>
-            <p>{drink_data["price"]}</p>
+            <div className="new-order-name">
+              <h2>
+                {drink_data["drink_name"]}
+              </h2>
+            </div>
+            <div className="new-order-price">
+              <p>
+                {drink_data["price"]} $
+              </p>
+            </div>
           </div>
         </div>
       );
@@ -68,6 +80,8 @@ const NewOrderList = (props) => {
 
 const NewOrderAction = () => {
 
+  const navigate = useNavigate();
+
   const handleSavedDrinks = () => {
     console.log("NewOrder::NewOrderAction.handleSavedDrinks - Clicked!");
   }
@@ -77,7 +91,7 @@ const NewOrderAction = () => {
   }
 
   return (
-    <div>
+    <div className="liquorish-footer">
       <button onClick={handleSavedDrinks}>Saved Drinks</button>
       <button onClick={handleMixYourOwn}>Mix Your Own!</button>
     </div>
@@ -116,8 +130,8 @@ const NewOrder = () => {
 
   useEffect(() => {
     getBarData().then((_bar_data) => {
-      getUserId().then((_user_id) => {      
-        console.log(bar_data)
+      getUserId().then((_user_id) => {
+        console.log(_bar_data)
         setBarData(_bar_data);
         setUserId(_user_id);
         setIsLoaded(true);
@@ -126,8 +140,13 @@ const NewOrder = () => {
   }, []);
 
   return (
-    <div>
-      <Header />
+    <div className="root">
+      <HeaderV2
+        does_nav={true}
+        nav_name={"Dashboard"}
+        nav_link={"/dashboard"}
+        title={"Ready to Order!"}
+      />
       {is_loaded &&
         <NewOrderList bar_id={bar_data["id"]} />
       }
