615497e6b4501e8d2b183a884695d27168102563
Anthony Mesa <amesa@hyperfix.digital>
2022-03-28 19:57:13 -0400

    added is_loaded guard state, created 3 functions to load client data, get saved drinks, and render those drinks to ui elements

--- src/SavedDrinks/SavedDrinks.js
+++ src/SavedDrinks/SavedDrinks.js
@@ -1,15 +1,48 @@
 
 import './SavedDrinks.css'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const SavedDrinks = () => {
 
   const [saved_drinks_dom, setSavedDrinksDom] = React.useState(null)
+  const [is_loaded, setIsLoaded] = React.useState(false)
+
+  const loadClientId = () => {
+    return new Promise((resolve, reject) => {
+      resolve()
+    });
+  }
+
+
+  const getSavedDrinks = () => {
+    return new Promise((resolve, reject) => {
+      resolve()
+    });
+  }
+
+
+  const generateSavedDrinksListDom = () => {
+    return new Promise((resolve, reject) => {
+      resolve()
+    });
+  }
+
+  useEffect(() => {
+    loadClientId().then((client_id) => {
+      getSavedDrinks(client_id).then((_saved_drinks) => {
+        generateSavedDrinksListDom(_saved_drinks).then((_saved_drinks_dom) => {
+          setSavedDrinksDom(_saved_drinks_dom)
+          setIsLoaded(true)
+        })
+      })
+    })
+  }, [])
 
   return (
     <div>
       SavedDrinks
-      { saved_drinks_dom }
+      {is_loaded &&
+      saved_drinks_dom }
     </div>
   );
 }
